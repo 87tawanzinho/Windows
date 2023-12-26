eca08ebe11fa9c0973a9c522c4c29d5fb7e6dbb8
tawan <87tawanzinho@gmail.com>
2023-12-26 02:11:10 -0300

    new changes

--- app/components/FoldersAndMore.tsx
+++ app/components/FoldersAndMore.tsx
@@ -1,8 +1,17 @@
 import Image from "next/image";
+import folder from "@/public/folder.png";
 export default function FoldersAndMore() {
   return (
-    <div>
-      <div></div>
+    <div className="flex justify-normal relative">
+      <div className="absolute w-32 hover:bg-gray-400 hover:opacity-75 hover:border-2 border-gray-600 text-white flex flex-col justify-center items-center cursor-default ">
+        <Image src={folder} alt="folder" />
+        <p className="text-xs">Windows Project</p>
+      </div>
+
+      <div className="absolute end-4 md:end-96 top-64 w-32 hover:bg-gray-400 hover:opacity-75 hover:border-2 border-gray-600 text-white flex flex-col justify-center items-center cursor-default ">
+        <Image src={folder} alt="folder" />
+        <p className="text-xs">Games</p>
+      </div>
     </div>
   );
 }
